c74eca603e9e53061a7ac233c3c9b8a7f7279648
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2025-01-02 17:18:36 +0530

    deref_trait

---
+++ pointers/deref-trait.rs
@@ -0,0 +1,16 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = MyBox(5);
+    println!("x: {}", *x); // Output: x: 5
+}
